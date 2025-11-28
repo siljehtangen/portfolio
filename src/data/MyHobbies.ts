3f0e19145b5809673048ad38f721325afb54f85c
siljehtangen <siljehtangen@outlook.com>
2025-11-29 00:56:40 +0100

    feat: update data for MyHobbies

--- src/data/MyHobbies.ts
+++ src/data/MyHobbies.ts
@@ -1,29 +1,29 @@
-import type { Hobby } from "@/types/hobby";
+import type { Hobby } from '@/types/hobby'
 
 export const hobbies: Hobby[] = [
   {
     title: 'Creative Writing',
     description:
       'Writing has strengthened my ability to communicate clearly, think logically, and approach problems with creativity and precision.',
-    skills: [
-      'Structured Thinking',
-      'Creativity',
-      'Attention to Detail',
-    ],
+    skills: ['Structured Thinking', 'Creativity', 'Attention to Detail'],
     devConnection:
       'Creative writing supports writing clean, maintainable code, effective documentation, and designing intuitive user experiences shaped by user narratives.',
   },
   {
     title: 'Football',
-    description: 'Playing football has taught me the value of teamwork, quick decision-making, and staying calm under pressure in competitive environments.',
+    description:
+      'Playing football has taught me the value of teamwork, quick decision-making, and staying calm under pressure in competitive environments.',
     skills: ['Teamwork', 'Strategic Thinking', 'Communication'],
-    devConnection: 'Working in teams, adapting to fast-changing situations, and making decisions quickly reflect many of the same dynamics found in projects I handle.',
+    devConnection:
+      'Working in teams, adapting to fast-changing situations, and making decisions quickly reflect many of the same dynamics found in the projects I work on.',
   },
   {
     title: 'Fitness & Training',
 
-    description: 'Regular training routines keep me focused and help build habits around discipline, consistency, and personal growth.',
+    description:
+      'Regular training routines keep me focused and help build habits around discipline, consistency, and personal growth.',
     skills: ['Consistency', 'Goal Setting', 'Resilience'],
-    devConnection: 'Staying consistent with fitness has taught me the value of small, steady improvements—just like the incremental progress needed to grow as a person, as part of a team, and in writing code.',
+    devConnection:
+      'Staying consistent with fitness has taught me the value of small, steady improvements—similar to the incremental progress needed to grow as a person, as part of a team, and in writing code.',
   },
 ]
