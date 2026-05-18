48d8c63572c901573b9de9ab5c95107fea198502
Silje Hanø Tangen <siljehtangen@outlook.com>
2026-05-18 20:11:22 +0200

    fix: remove comment

--- src/components/MyNavbar.vue
+++ src/components/MyNavbar.vue
@@ -107,7 +107,6 @@
         </AppTooltip>
       </div>
 
-      <!-- Scroll-depth progress bar -->
       <div class="absolute bottom-0 left-0 right-0 h-0.5 overflow-hidden bg-[var(--bg-secondary)]">
         <div
           class="h-full transition-[width] duration-100 ease-out"
